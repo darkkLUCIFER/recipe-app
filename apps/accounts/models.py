7135f98c6cc26e94d4e645bc26ed2211272802f1
sorena <mdn1376@gmail.com>
2024-11-01 19:05:11 +0330

    feat: added custom user model

--- apps/accounts/models.py
+++ apps/accounts/models.py
@@ -1,3 +1,16 @@
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 from django.db import models
 
-# Create your models here.
+from apps.accounts.managers import CustomUserManager
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    """Custom user model that supports using email instead of username."""
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = CustomUserManager()
+
+    USERNAME_FIELD = 'email'
--- apps/accounts/tests/test_models.py
+++ apps/accounts/tests/test_models.py
@@ -15,3 +15,36 @@ class ModelTests(TestCase):
 
         self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
+
+    def test_new_user_email_normalized(self):
+        """Test the email for a new user is normalized."""
+        email = 'test@GMAIL.COM'
+        user = get_user_model().objects.create_user(
+            email=email,
+            password='pass123'
+        )
+
+        self.assertEqual(user.email, email.lower())
+
+    def test_new_user_invalid_email(self):
+        """Test creating user with no email raises error."""
+
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user(
+                email=None,
+                password='pass123'
+            )
+
+    def test_create_new_superuser(self):
+        """Test creating a new superuser."""
+        email = 'mdn1376@gmail.com'
+        password = 'pass123'
+
+        super_user = get_user_model().objects.create_superuser(
+            email=email,
+            password=password
+        )
+
+        self.assertEqual(super_user.email, email)
+        self.assertTrue(super_user.is_superuser)
+        self.assertTrue(super_user.is_staff)
