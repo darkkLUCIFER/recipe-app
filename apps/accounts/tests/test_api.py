ef055130fb5371b5c23ef1f3c63f106cb176c4dd
sorena <mdn1376@gmail.com>
2024-11-05 20:12:37 +0330

    feat: made CreateTokenView

--- apps/accounts/tests/test_api.py
+++ apps/accounts/tests/test_api.py
@@ -5,6 +5,7 @@ from rest_framework.test import APITestCase, APIClient
 from rest_framework import status
 
 CREATE_USER_URL = reverse('accounts:create')
+TOKEN_URL = reverse('accounts:token')
 
 
 def create_user(**params):
@@ -62,3 +63,55 @@ class PublicUserApiTests(APITestCase):
             email=payload['email']
         ).exists()
         self.assertFalse(user_exists)
+
+    def test_create_token_for_user(self):
+        """Test that a token is created for the user"""
+        payload = {
+            'email': 'test@gmail.com',
+            'password': '123pass',
+        }
+
+        create_user(**payload)
+
+        response = self.client.post(TOKEN_URL, payload)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertIn('token', response.data)
+
+    def test_create_token_invalid_credentials(self):
+        """Test that token is not created if invalid credentials are given"""
+        create_user(email='test@gmail.com', password='pass123')
+        payload = {
+            'email': 'test@gmail.com',
+            'password': 'wrong_pass',
+        }
+
+        response = self.client.post(TOKEN_URL, payload)
+
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertNotIn('token', response.data)
+
+    def test_create_token_no_user(self):
+        """Test that token is not created if user doesn't exist'"""
+        payload = {
+            'email': 'test@gmail.com',
+            'password': 'pass123',
+        }
+
+        response = self.client.post(TOKEN_URL, payload)
+
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertNotIn('token', response.data)
+
+    def test_create_token_missing_fields(self):
+        """Test that email and password are required"""
+
+        payload = {
+            'email': 'test@gmail.com'
+        }
+
+        response = self.client.post(TOKEN_URL, payload)
+
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertIn('password', response.data)
+        self.assertNotIn('token', response.data)
--- apps/accounts/views.py
+++ apps/accounts/views.py
@@ -1,7 +1,14 @@
+from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.generics import CreateAPIView
+from rest_framework.settings import api_settings
 
-from apps.accounts.serializers import UserSerializer
+from apps.accounts.serializers import CreateUserSerializer, AuthTokenSerializer
 
 
 class CreateUserView(CreateAPIView):
-    serializer_class = UserSerializer
+    serializer_class = CreateUserSerializer
+
+
+class CreateTokenView(ObtainAuthToken):
+    serializer_class = AuthTokenSerializer
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
